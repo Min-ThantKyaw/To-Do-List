b03d3adec059385e16960106bc513bb1b21e6057
Min Thant Kyaw <minthant1590@gmail.com>
2026-07-14 00:07:08 +0900

    fix: Code Organization and refactor

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
-const themeToggleBtn = document.getElementById("themeToggle");
-const sidebarToggleBtn = document.getElementById("sidebarToggle");
+/**
+ * DOM Selector
+ */
+const html = document.documentElement;
 const sidebar = document.getElementById("sidebar");
 const backdrop = document.getElementById("sidebarBackdrop");
-const html = document.documentElement;
 
-//UI state
+// Buttons
+const themeToggleBtn = document.getElementById("themeToggle");
+const sidebarToggleBtn = document.getElementById("sidebarToggle");
+const toggelCompletedBtn = document.getElementById("toggleCompleted");
+const saveTaskBtn = document.getElementById("saveTaskBtn");
+const addTaskModal = document.getElementById("addTaskModal");
+const cancelTaskBtn = document.getElementById("cancelTaskBtn");
+
+//Container and component
+const completedTasksContainer = document.getElementById("completedList");
+const completedArrow = document.getElementById("completedArrow");
+
+/**
+ * UI State
+ */
 let isSidebarOpen = localStorage.getItem("sidebarState") === "true";
 let theme = localStorage.getItem("theme") || "light";
 let isMobileView = window.innerWidth < 1024;
+let isCompletedListOpen = true;
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-//Initialize theme and sidebar
+/**
+ * Initialize theme and sidebar
+ */
 function initTheme() {
   html.classList.toggle("dark", theme === "dark");
 }
 
-//Initialize sidebar
 function initSidebar() {
   if (isSidebarOpen) {
     if (isMobileView) {
@@ -30,43 +48,33 @@ function initSidebar() {
   }
 }
 
-//Toggle sidebar
 function sidebarToggle() {
   isSidebarOpen = !isSidebarOpen;
   localStorage.setItem("sidebarState", isSidebarOpen);
   initSidebar();
 }
 
-//Toggle theme
 function themeToggle() {
   theme = theme === "light" ? "dark" : "light";
   localStorage.setItem("theme", theme);
   initTheme();
 }
 
-//Event listeners
-themeToggleBtn.addEventListener("click", themeToggle);
-sidebarToggleBtn.addEventListener("click", sidebarToggle);
-backdrop.addEventListener("click", sidebarToggle);
-window.addEventListener("resize", () => {
-  isMobileView = window.innerWidth < 1024;
-  initSidebar();
-});
+function completeListToggle() {
+  isCompletedListOpen = !isCompletedListOpen;
+  if (!isCompletedListOpen) {
+    completedTasksContainer.classList.add('hidden');
+    completedArrow.classList.replace('ph-caret-down', 'ph-caret-up');
+  } else {
+    completedTasksContainer.classList.remove('hidden');
+    completedArrow.classList.replace('ph-caret-up', 'ph-caret-down');
+  }
+}
 
-//Initialize theme and sidebar
-initTheme();
-initSidebar();
-
-//Task Modal
-
-//Add Tasks
+/**
+ * Task Modal and CRUD Logic
+ */
 const taskForm = document.getElementById("taskForm");
-
-//Add Task button
-const saveTaskBtn = document.getElementById("saveTaskBtn");
-const addTaskModal = document.getElementById("addTaskModal");
-const cancelTaskBtn = document.getElementById("cancelTaskBtn");
-let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
 function openModal() {
   addTaskModal.classList.remove("hidden");
@@ -143,6 +151,7 @@ function validateForm() {
 
   return { valid: errors.length === 0, errors };
 }
+
 function getTaskById(taskId) {
   return tasks.find((t) => t.id === taskId);
 }
@@ -170,10 +179,12 @@ function saveTask(task) {
   localStorage.setItem("tasks", JSON.stringify(tasks));
   closeModal();
 }
+
 function deleteTask(taskId) {
-    tasks = tasks.filter((task) => task.id !== taskId);
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+  tasks = tasks.filter((task) => task.id !== taskId);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
+
 function toggleCompletedStatus(taskId) {
   const task = getTaskById(taskId);
   if (task) {
@@ -184,10 +195,13 @@ function toggleCompletedStatus(taskId) {
 }
 
 function renderTasks() {
+  const pendingTasks = tasks.filter((task) => !task.completed);
   const completedTasks = tasks.filter((task) => task.completed);
   const tasksContainer = document.getElementById("taskList");
+  const completedTasksContainer = document.getElementById("completedList");
   tasksContainer.innerHTML = "";
-  tasks.forEach((task) => {
+  completedTasksContainer.innerHTML = "";
+  pendingTasks.forEach((task) => {
     const li = document.createElement("li");
     li.setAttribute("data-id", task.id);
     li.className =
@@ -238,16 +252,54 @@ function renderTasks() {
                                         <button class="delete-btn rounded-lg p-2 opacity-0 group-hover:opacity-100 hover:bg-gray-100 dark:hover:bg-gray-700">
                                             <i class="ph ph-pencil"></i>
                                         </button>
-                                    </did>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                         </li>`;
     tasksContainer.appendChild(li);
   });
-    
-    completedTasks.map(task)
+  console.log(completedTasks)
+  completedTasks.forEach(completedTask => {
+    const li = document.createElement("li");
+    li.setAttribute("data-id", completedTask.id);
+    li.className =
+      "flex items-center justify-between rounded-lg border border-gray-200 dark:border-gray-700 bg-gray-50 dark:bg-gray-900 px-4 py-3";
+    li.innerHTML = `
+                <div class="flex items-center gap-3">
+                  <input
+                    type="checkbox"
+                    ${completedTask.completed ? "checked" : ""}
+                    class="h-5 w-5 rounded text-primary focus:ring-primary"
+                  />
+
+                  <div>
+                    <p class="line-through text-gray-400">
+                      ${completedTask.title}
+                    </p>
+                  </div>
+                </div>
+
+                <button class="delete-btn text-gray-400 hover:text-red-500">
+                  <i class="ph ph-trash"></i>
+                </button>
+              </li>`;
+    completedTasksContainer.appendChild(li);
+  })
+
+
 }
+
+//Event listeners
+themeToggleBtn.addEventListener("click", themeToggle);
+sidebarToggleBtn.addEventListener("click", sidebarToggle);
+backdrop.addEventListener("click", sidebarToggle);
+toggelCompletedBtn.addEventListener("click", completeListToggle);
+cancelTaskBtn.addEventListener("click", closeModal);
+window.addEventListener("resize", () => {
+  isMobileView = window.innerWidth < 1024;
+  initSidebar();
+});
 taskForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const validation = validateForm();
@@ -260,24 +312,25 @@ taskForm.addEventListener("submit", (event) => {
   saveTask(task);
   renderTasks();
 });
-cancelTaskBtn.addEventListener("click", closeModal);
 document.addEventListener("click", (event) => {
   const target = event.target;
-  const listIem = target.closest("li[data-id]");
-  if (!listIem) return;
+  const listItem = target.closest("li[data-id]");
+  if (!listItem) return;
 
-  const taskId = listIem.getAttribute("data-id");
+  const taskId = listItem.getAttribute("data-id");
 
-    if (target.type === "checkbox") {
-        console.log(taskId);
-        toggleCompletedStatus(taskId);
-        renderTasks();
-    } else if (target.closest(".delete-btn")) {
-        deleteTask(taskId);
-        renderTasks();
+  if (target.type === "checkbox") {
+    console.log(taskId);
+    toggleCompletedStatus(taskId);
+    renderTasks();
+  } else if (target.closest(".delete-btn")) {
+    deleteTask(taskId);
+    renderTasks();
   }
 });
+
 window.addEventListener("DOMContentLoaded", () => {
   renderTasks();
+  initTheme();
+  initSidebar();
 });
-console.log(tasks);
