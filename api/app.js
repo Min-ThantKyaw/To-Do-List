5a1b165041f8000189a2e1b9da3c8901dc69b29c
Min Thant Kyaw <minthant1590@gmail.com>
2026-07-27 14:28:03 +0900

    feat: Integrate global error handling middleware for improved error management

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const responseHandler = require('./src/middleware/responseHandler');
+const errorHandler = require('./src/middleware/globalErrorHandler');
 //route files
 const tasksRoutes = require('./src/routes/tasks.routes');
 // const categoriesRoutes = require('./src/routes/categories.routes');
@@ -13,5 +14,5 @@ app.use(express.json()); // Parse JSON request bodies
 app.use(responseHandler);
 app.use('/api', tasksRoutes);
 
-
+app.use(errorHandler);
 module.exports = app;
